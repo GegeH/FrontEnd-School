954ac6e108f8a204186d145bcbb9d44d06d32699
GegeH <gegehuang.gh@gmail.com>
2017-07-09 22:04:26 -0400

    update HW

---
+++ HW/HW/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using HW.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace HW.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [EnableCors("http://localhost:5794", "*", "GET")]
+        public IEnumerable<Person> Get()
+        {
+            string filename = @"C:\Users\Gege\Documents\Visual Studio 2015\Projects\DotNET_InClass\FrontEnd_InClass\FProj\FrontEnd-School\HW\HW\data.txt";
+            string text = System.IO.File.ReadAllText(filename);
+            var person = JsonConvert.DeserializeObject<List<Person>>(text);
+            return person;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [EnableCors("*", "*", "GET,POST")]
+        public bool Post([FromBody]Person value)
+        {
+            return false;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HW/HW/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace HW.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Role { get; set; }
+        // 0 : student
+        // 1 : teacher
+        public string Password { get; set; }
+
+    }
+}
