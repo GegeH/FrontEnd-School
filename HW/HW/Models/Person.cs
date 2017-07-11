f0aac85dc83ff64d478e701c63931491201cf6e8
Xinyi <xie_xin_yi@hotmail.com>
2017-07-11 10:17:46 -0400

    delete user name, add correctnumber and progress

--- HW/HW/Models/Person.cs
+++ HW/HW/Models/Person.cs
@@ -8,14 +8,16 @@ namespace HW.Models
 {
     public class Person
     {
+        // use Id to log in
         public int Id { get; set; }
-        public string UserName { get; set; }
+      //  public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Role { get; set; } // 0 : student, 1 : teacher
         public string Password { get; set; }
-        public int Login { get; set; } // 0: not logged in, 1: is doing testing 
+       // public int Login { get; set; } // 0: not logged in, 1: is doing testing 
 
+       // initial to 0 if it is in a database
         public int Progress { get; set; } // how many tests already completed
         public int CorrectNumber { get; set; } // how many tests is correct
     }
